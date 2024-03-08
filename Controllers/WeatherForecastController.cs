6739fa2bb001393780c1072da63d20783b7d8596
cent-dxv <centdxv@gmail.com>
2024-03-08 15:12:18 +0300

    auto_Controllers/WeatherForecastController.cs

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,63 @@
+using auth_final_101.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace auth_final_101.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+
+    [HttpGet("/")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 2).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpGet("user")]
+    [Authorize]
+    public IEnumerable<WeatherForecast> Get_user()
+    {
+        return Enumerable.Range(1, 2).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+
+    [HttpGet("admin")]
+    [Authorize(Roles = "admin")]
+    public IEnumerable<WeatherForecast> Get_admin()
+    {
+        return Enumerable.Range(1, 10).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
