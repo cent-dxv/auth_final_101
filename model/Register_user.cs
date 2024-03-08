c2833bbeee766275273ce6f083bca7f8ab4e3e0e
cent-dxv <centdxv@gmail.com>
2024-03-08 15:13:11 +0300

    auto_model/Register_user.cs

---
+++ model/Register_user.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace auth_final_101.Models;
+
+    public class UserRegester
+    {
+        public string? Id { get; set; } = string.Empty;
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+
+
+    public class LoginUser
+    {
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
